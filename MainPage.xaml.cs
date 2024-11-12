0453a652f0c24aed5ee86d1d399d7a85a879573f
MbYesMbNo <akamegakill228@mail.ru>
2024-11-12 12:10:25 +0500

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ToDo.ToDoTasks;
+
+namespace ToDo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainViewModel();
+            InitializeComponent();
+           
+        }
+
+    }
+ 
+    class MainViewModel : INotifyPropertyChanged {
+        public Command AddCommand { get; }
+        public Command DelCommand { get; set; }
+        public ObservableCollection<ToDoTasks.ToDoTasks> Tasks { get; } = new ObservableCollection<ToDoTasks.ToDoTasks>();
+
+        private ToDoTasks.ToDoTasks _task = new ToDoTasks.ToDoTasks();
+
+        public ToDoTasks.ToDoTasks SelectedTask
+        {
+            get => _task;
+            set
+            {
+                if (_task == value)
+                    return;
+                if (value != null)
+                   _task = value;
+                OnPropertyChanged(nameof(SelectedTask));
+            }
+        }
+        public MainViewModel()
+        {
+
+            AddCommand = new Command(() =>
+            {
+                Tasks.Add(new ToDoTasks.ToDoTasks { TaskText = taskText, IsCompletedTask=false });
+                taskText = string.Empty;
+            }, () => !string.IsNullOrWhiteSpace(taskText));
+
+            DelCommand = new Command<ToDoTasks.ToDoTasks>((DeleteTask) =>
+            {
+                Tasks.Remove(DeleteTask);
+            }, (DeleteTask) => SelectedTask!=null);
+        }
+        
+        public string taskText {
+            get => _task.TaskText;
+            set
+            {
+                if (value == _task.TaskText)
+                    return;
+                if(value!=null)
+                    _task.TaskText = value;
+                OnPropertyChanged(nameof(taskText));
+            }
+        }
+
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    
+}
---
+++ ToDoTasks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.ToDoTasks {
+    public class ToDoTasks : INotifyPropertyChanged
+    {
+        private string _taskText;
+        private bool _isCompletedTask;
+        public string TaskText {
+            get=>_taskText; set {
+                if (_taskText == value)
+                    return;
+                _taskText = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(_taskText)));
+            }
+        }
+        public bool IsCompletedTask { 
+            get => _isCompletedTask; set
+            {
+                if (_isCompletedTask == value) return;
+                _isCompletedTask = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_isCompletedTask)));
+            }
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
+
+
